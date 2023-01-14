04227a3df1f7149fcbb498571175972b6746485b
Cristian David el mejor nombre <esunaprueba@gmail.com>
2023-01-14 14:38:48 -0500

    Strings

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 print("******")
+# name1 = "Cristian   "
+# text = "ana"
 nombre = input("Ingrese su nombre ")
-apellido = input("Ingrese su apellido ")
-edad = input("Ingrese su edad ")
-
-fullname = "mi nombres es" + nombre + " mi apellido es" + apellido + " y tengo una edad de " + edad 
-
-print(fullname)
+# print(name1.upper())
+# print(name1 == nombre)
+# print(len(name1))
+# fulltext = text + nombre
+# fulltextv1 = f"{text} {nombre} ..!"
+# print(fulltext)
+# print(fulltextv1)
+print( nombre == nombre[::-1])
