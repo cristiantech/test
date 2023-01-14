93609a6d8d1ea8f791de774ae7c0f3d05d76c008
Cristian David el mejor nombre <esunaprueba@gmail.com>
2023-01-14 12:30:09 -0500

    empezando con git

---
+++ main.py
@@ -0,0 +1,8 @@
+print("******")
+nombre = input("Ingrese su nombre ")
+apellido = input("Ingrese su apellido ")
+edad = input("Ingrese su edad ")
+
+fullname = "mi nombres es" + nombre + " mi apellido es" + apellido + " y tengo una edad de " + edad 
+
+print(fullname)
